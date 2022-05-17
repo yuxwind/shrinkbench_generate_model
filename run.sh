1d3f1e0475747779bfaa8d6edfaadab847f40767
yuxwind <yuxwind@gmail.com>
2022-05-17 17:19:55 -0600

    add run.sh, env.yml

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dataset=$1
+arch=$2
+round=$3
+gpu=$4
+log=logs/$dataset-$arch-round$round.log
+echo $log
+python generate_models.py  $dataset $arch $round $gpu > $log 2>&1 &
+tail -f $log
