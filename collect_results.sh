e920a822cd9690bce9b758a9497e6e5ff9b84609
yuxwind <yuxwind@gmail.com>
2022-05-18 23:59:30 -0600

    sync  collect_results.sh

--- collect_results.sh
+++ collect_results.sh
@@ -12,19 +12,20 @@ if [ "$server" = "longclaw" ]; then
     cp $remote/shrinkbench/saved_models/*$arch*  $arch/saved_models/
     cp $remote/shrinkbench/results/$arch/*   $arch/results/
 elif [ "$server" = "bluefish" ]; then
+    # run this on bluefish...
     #remote=jcao@bluefish:/uusoc/exports/scratch/jcao/echo/shrinkbench
     remote=longclaw:/uusoc/exports/scratch/xiny/project/shrinkbench/Aidan/shrinkbench/sync_results/    
     echo $arch
     echo $remote
     scp -r ./saved_models/*$arch* $remote/$arch/saved_models/
-    scp -r ./results/$arch/* $remote/$arch/results/
+    scp -r ./results/$arch*/* $remote/$arch/results/
 elif [ "$server" = "redfish" ]; then
     #remote=jcao@redfish:/uusoc/exports/scratch/jcao/echo/shrinkbench
     remote=longclaw:/uusoc/exports/scratch/xiny/project/shrinkbench/Aidan/shrinkbench/sync_results/    
     echo $arch
     echo $remote
     scp -r saved_models/*$arch* $remote/$arch/saved_models/
-    scp -r results/$arch/* $remote/$arch/results
+    scp -r results/$arch*/* $remote/$arch/results
 else  
     if [ "$server" = "thor" ]; then
         remote=thor:/home/thor/xin
@@ -39,5 +40,5 @@ else
     echo $arch
     echo $remote
     scp -r $remote/shrinkbench/saved_models/*$arch*  $arch/saved_models/
-    scp -r $remote/shrinkbench/results/$arch/*   $arch/results/
+    scp -r $remote/shrinkbench/results/$arch*/*   $arch/results/
 fi
